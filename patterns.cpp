792a4e795a6ac2ca831800db78190483894222db
Prince Tomar <prince@farmsetu.co>
2024-08-13 06:07:47 +0530

    pattern 3 done

--- patterns.cpp
+++ patterns.cpp
@@ -25,9 +25,21 @@ void pattern2(int n)
     }
 }
 
+void pattern3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    pattern2(n);
+    pattern3(n);
 }
