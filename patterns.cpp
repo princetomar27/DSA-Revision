a7e4f21b4c029d36c8804938af3843d8ee75c3ae
Prince Tomar <prince@farmsetu.co>
2024-08-13 06:43:53 +0530

    Completed upto patter 9

--- patterns.cpp
+++ patterns.cpp
@@ -73,9 +73,59 @@ void pattern6(int n)
     }
 }
 
+void pattern7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // for space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        // for space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern8(int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // for space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        // for space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern9(int n)
+{
+    pattern7(n);
+    pattern8(n);
+}
+
 int main()
 {
     int n;
     cin >> n;
-    pattern6(n);
+    pattern9(n);
 }
