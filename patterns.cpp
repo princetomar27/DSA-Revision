a9aa5e316dc2b30c99dd62e99dcb341490802e96
Prince Tomar <prince@farmsetu.co>
2024-08-13 06:03:00 +0530

    patter2 done

---
+++ patterns.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << '*' << ' ';
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    pattern2(n);
+}
