d39f589c4960d0c3db724b1096564ff1c03b986d
Prince Tomar <prince@farmsetu.co>
2024-08-14 06:19:00 +0530

    pattern 12 done

--- patterns.cpp
+++ patterns.cpp
@@ -161,9 +161,35 @@ void pattern11(int n)
     }
 }
 
+void pattern12(int n)
+{
+    int space = 2 * (n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+
+        // nums
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        // nums 2
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    pattern11(n);
+    pattern12(n);
 }
