7b2d5f0c047af6442d83c0dbce0ee2b2922609c7
Prince Tomar <prince@farmsetu.co>
2024-08-14 06:32:01 +0530

    Pattern 16 done

--- patterns.cpp
+++ patterns.cpp
@@ -213,9 +213,35 @@ void pattern14(int n)
     }
 }
 
+void pattern15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch < 'A' + (n - i); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n)
+{
+    char ch = 'A';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+        ch += 1;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    pattern14(n);
+    pattern16(n);
 }
