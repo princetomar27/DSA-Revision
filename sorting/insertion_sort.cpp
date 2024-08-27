ee8065051e3f086c49022ce1c9f1d26beedcf231
Prince Tomar <prince@farmsetu.co>
2024-08-27 06:12:36 +0530

    Insertion sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            // swap
+            int t = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = t;
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
